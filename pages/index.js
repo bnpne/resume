9bbb89f5d31bd64473b8aa4ee11107a29754c50c
bnpne <bentppaine@gmail.com>
2021-03-11 12:28:03 -0800

    meta

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,63 @@
 import React from 'react'
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import useRouter from 'next/router'
 
-const Index = () => {
+const Index = ({meta: pageMeta}) => {
+  const router = useRouter()
+
+  const meta = {
+    title: 'Ben Paine',
+    description: 'Front End Developer',
+    cardImage: 'https://bnpne.io/bnpne.png',
+    keywords: [
+      'technology',
+      'software',
+      'web',
+      'development',
+      'blog',
+      'startup',
+      'front end',
+      'creative',
+      'interactive designer',
+      'designer',
+    ],
+    ...pageMeta,
+  }
   return (
     <div className="">
       <Head>
-        <title>Ben Paine</title>
+        <title>{meta.title}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta content={meta.description} name="description" />
+        <meta content={meta.keywords.join(', ')} name="keywords" />
+        <meta property="og:url" content={`https://resume.bnpne.io${router.asPath}`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Ben Paine" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.cardImage} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@bnpneio" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.cardImage} />
       </Head>
       <div className="px-3 py-2 w-full mx-auto text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-light tracking-tight space-y-12">
         <div>
@@ -91,7 +142,7 @@ const Index = () => {
             (See Full Resume)
           </a>
         </div>
-      <Footer />
+        <Footer />
       </div>
     </div>
   )
